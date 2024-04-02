7a67c2aae6f1502112d185c3f7237608b14ab9e8
Michael <michael.sim132@gmail.com>
2024-04-02 02:29:13 -0400

    update rollup for env vars on render

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -5,7 +5,7 @@ import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
-import dotenv from 'rollup-plugin-dotenv';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -39,7 +39,10 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
-		dotenv(),
+		replace({
+			preventAssignment: true,
+			'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL),
+		  }),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
