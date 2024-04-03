451b06bc716aa0a348119b5eca98d37a52c8ddc6
Michael <michael.sim132@gmail.com>
2024-04-02 20:56:03 -0400

    rollback and remove useless lib

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -5,7 +5,6 @@ import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
-import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -39,10 +38,6 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
-		replace({
-			preventAssignment: true,
-			'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL),
-		  }),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
